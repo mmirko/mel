0309b4c1ca79a9f93009743f61a05be1ce3d0d84
Mirko Mariotti <mirko@mirkomariotti.it>
2022-04-27 13:15:25 +0200

    Added conconi

---
+++ conconi/conconi_test.go
@@ -0,0 +1,30 @@
+package conconi
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/mmirko/mel"
+)
+
+func TestActions(t *testing.T) {
+
+	rand.Seed(int64(time.Now().Unix()))
+
+	fmt.Println("---- Test: Conconi ----")
+
+	x := []float32{100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600}
+	y := []float32{70, 80, 90, 100, 110, 120, 130, 140, 150, 151, 152, 153, 154, 155, 156, 157}
+
+	var ep *mel.EvolutionParameters
+	for i := 0; i < 1000; i++ {
+		ctest := ConconiGenerate(ep)
+		mutation := ConconiMutate(ctest, ep)
+		value, _ := ConconiFitness(ctest.(*Conconi), x, y)
+		fmt.Println("Generated: [", ctest, "] - Fitness: [", value, "] - Mutated: [", mutation, "]")
+	}
+
+	fmt.Println("---- End test ----")
+}
