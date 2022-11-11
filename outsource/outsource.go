0bc592db7766fed1ed66dfe31baf03f907a10a1e
Mirko Mariotti <mirko@mirkomariotti.it>
2022-11-11 23:45:19 +0100

    Minor

--- outsource/outsource.go
+++ outsource/outsource.go
@@ -1,5 +1,11 @@
 package outsource
 
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
 type OutSource interface {
 	SendGenotype() ([]byte, error)
 	ReceiveGenotype([]byte) error
@@ -7,4 +13,22 @@ type OutSource interface {
 
 type outSourceServer struct {
 	waitList chan []byte
+}
+
+func (m *outSourceServer) newFenoHandler(w http.ResponseWriter, r *http.Request) {
+	result := ""
+	result += `
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>m</title>
+<body>
+</body>
+</html>
+`
+	io.WriteString(w, result)
+}
+
+func (m *outSourceServer) Serve() {
+	http.HandleFunc("/newfenotype", m.newFenoHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
